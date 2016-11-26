3d0c1c822f8df3e098f3d8d1042703a2c3f63f85
Elise Weir <enw23@cornell.edu>
2016-11-26 11:16:03 -0500

    Implementation of states being visualized initially. Compiles on my local repo

--- fsm_class.ml
+++ fsm_class.ml
@@ -1,4 +1,4 @@
-(*Types and inputs from Machine : Begin *)
+(*Types and inputs from Machine : Begin
 
 type state = Q of int
 
@@ -12,18 +12,32 @@ let init = {all_states = [Q 0 ; Q 1] ; prev_state = Q 0;
                curr_state = Q 1; next_state = Q 1 ; alphabet = [0;1];
                tape_symbol_read = Sigma 0}
 
-(* End *)
+ End *)
 
 open GMain
 open GdkKeysyms
 open GObj
+open Main_gui
 
 
 (* Create FSM in initial state *)
 
+(* Filled, black-outlined rectangle. *)
+let draw_rectangle (drawable : GDraw.drawable)
+    fill_col (x, y) squ_length =
+
+  drawable#set_foreground (`NAME fill_col);
+  drawable#rectangle ~x:x ~y:y ~width: squ_length
+  ~height: squ_length ~filled:true ();
+
+  drawable#set_foreground `BLACK;
+  drawable#rectangle ~x:x ~y:y ~width: squ_length
+  ~height: squ_length ~filled:false ()
+
+
 (* [fsm status] class that inputs a status record and implements the methods
 needed to the create a visualization of the FSM. *)
-class fsm_class status ?packing ?show ?width ?height ?packing ?show =
+class fsm (status : Main_gui.machine ) ?packing ?show ?width ?height ?packing ?show =
 
 (* Create the containing vbox. *)
   let vbox = GPack.vbox ?width ?height ?packing ?show () in
@@ -42,6 +56,51 @@ class fsm_class status ?packing ?show ?width ?height ?packing ?show =
 
 
     (* Methods *)
+    method init_paint () =
+
+      (* Create Overall Ractangle to draw on *)
+      let drawable_i = Lazy.force drawable in
+      let (width_i, height_i) = drawable_i#size in
+      let (float_width_i, float_height_i) =
+      (float_of_int(width_i), float_of_int(height_i)) in
+
+      (* Calculate Size of Rectangle *)
+      let state_num = List.length status.all_states in
+      let tot_state_area = width_i*height_i/2 in
+      let state_area = tot_state_area/state_num in
+      (* Problem: Need operator - Square-root of state_area *)
+      let state_length = state_area |> float_of_int |> sqrt |> int_of_float in
+
+      (* Draw Rectangles on screen - x/y-coordinates + color *)
+      let a = int_of_float(0.1*.float_of_int(width_i)) in
+      let b =  int_of_float(0.1*.float_of_int(height_i)) in
+
+
+      for n = 0 to (state_num - 1) do
+
+         (* Still to do - color, (a, b) coordinates *)
+
+        draw_rectangle drawable_i "aqua" (a, b) state_length ;
+
+        (* Draw Text *)
+        let float_a = float_of_int(a) in
+        let float_b = float_of_int(b) in
+
+        (* New X Y Coordinates *)
+        if float_b >= 0.9*.float_width_i then
+          let a = int_of_float(0.1*.float_width_i) in
+          let b = b + state_length + int_of_float(0.03*.float_height_i) in
+          (a, b)
+
+        else
+          let a = a + state_length + int_of_float(0.03*.float_width_i) in
+          (a, b)
+
+      done
+
+
+
+
 
 
     (* - Draw Rectangle
