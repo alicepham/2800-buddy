abf13c13cfc4ed7c1d7714963461fc19ffbdee1f
Tavish McDonald <tmm248@cornell.edu>
2016-11-28 19:51:43 -0500

    updated machine backend

--- machine.ml
+++ machine.ml
@@ -1,52 +1,57 @@
 
-
-
+(**)
 type direction = Left | Right
 
-type state = Q of int
+(**)
+type state =  Q of int | None
 
-type symbol = Blank | Sigma of int
-
-type transition_function = state * symbol * direction list list
+type transition_function = state * string * state *string * direction list
 
 type machine = {all_states : state list ; prev_state : state ;
-               curr_state: state; next_state : state ; alphabet : int list ;
-               tape_symbol_read : symbol}
+               curr_state: state; next_state : state ; alphabet : string list ;
+               tape_char : string}
 
+let rec remove_dups lst =
+  match lst with
+  | [] -> []
+  | h::t -> if List.mem h t then remove_dups t
+            else h :: remove_dups t
 
-let matrix = [[(Q 0, Sigma 1, Right); (Q 1, Sigma 0, Right)];
-               [(Q 1, Sigma 1, Left); (Q 0, Blank, Right)]]
+let rec get_states lst =
+  match lst with
+  | [] -> []
+  | ((x,y), (a,b,c))::t -> x::(get_states t)
 
-let machine = {all_states = [Q 0 ; Q 1] ; prev_state = Q 0;
-               curr_state = Q 1; next_state = Q 1 ; alphabet = [0;1];
-               tape_symbol_read = Sigma 0}
+let rec get_alphabet lst =
+  match lst with
+  | [] -> []
+  | ((x,y),(a,b,c))::t -> y::(get_alphabet t)
 
-(*[state_to_num state] unwraps a state to its int value for use in transition
- * function matrix lookup*)
-let state_to_num state =
-  match state with
-  | Q n -> n
+  let rec position machine lst =
+  match lst with
+  | [] -> 0
+  | ((x,y),(a,b,c))::t -> if machine.curr_state = x then 0
+                          else
+                            1 + position machine t
 
-(* [symbol_to_num symbol] unwraps a symbol to its int value for transition
- * function matrix lookup. Here 2 corresponds to the Symbol column for the
- * Blank symbol*)
-let symbol_to_num symbol =
-  match symbol with
-  | Sigma x -> x
-  | Blank -> 2
+let rec find_next_state k = function
+  | [] -> raise (Failure "Machine is in final state")
+  | ((x,y),(a,b,c))::t -> if k = 0 then x else find_next_state (k-1) t
 
-(*[step mach] Steps a turing machine from its current state to the next
- * as determined by the trasition function matrix.
- * Requires
- *  - mach is a valid machine type*)
+let rec transform machine matrix =
+  match matrix with
+  | [] -> raise (Failure "Not Found")
+  | ((x,y),(a,b,c))::t -> if machine.curr_state = x &&
+                                       machine.tape_char = y then (a,b,c)
+                          else
+                            transform machine t
+
 let step mach =
-let x_comp = state_to_num (mach.curr_state) in
-let y_comp = symbol_to_num(mach.tape_symbol_read) in
-let move = List.nth (List.nth matrix x_comp) y_comp in
-match move with
-| (x,y,z) -> {all_states = [Q 0 ; Q 1] ; prev_state = mach.curr_state;
-               curr_state = x; next_state = Q 1 ; alphabet = [0;1];
-               tape_symbol_read = y}
-
-
-
+  let update = transform mach matrix in
+  match update with
+  | (x,y,z) -> {all_states = remove_dups(get_states matrix );
+                prev_state = mach.curr_state;
+                curr_state = x; next_state =
+                find_next_state ((position mach matrix)+1) matrix;
+                alphabet = remove_dups(get_alphabet matrix) ;
+                tape_char = y}
