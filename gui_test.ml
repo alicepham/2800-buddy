fabc946a32fd5da87963f5a465089225a93af587
Elise Weir <enw23@cornell.edu>
2016-11-29 19:23:39 -0500

    Pre-integration FSM visualization commit

--- gui_test.ml
+++ gui_test.ml
@@ -23,20 +23,21 @@ let locale = GtkMain.Main.init ()
 
 type state_location = { state : state ; x_cent : int ; y_cent : int }
 
-(* Create FSM in initial state
-let font_name = "-*-helvetica-*--120-*"
+(* Create FSM in initial state *)
+let font_name = "-arabic-newspaper-medium-*-*-*-*-120-*-*-*-*-*-*"
 
 let font =
   try
     Gdk.Font.load font_name
   with
     Gpointer.Null -> failwith ("font " ^ font_name ^ ": not found")
-*)
+
 
 (* Draw Text on drawable object *)
 let draw_text drawable (x, y) font text =
   let string_width = Gdk.Font.string_width font text in
   let string_height = Gdk.Font.string_height font text in
+  let () = Pervasives.print_string "draw text" in
   drawable#string text ~font ~x:(x - string_width/2) ~y:(y+string_height)
 
 
@@ -69,11 +70,16 @@ let main () =
   (* Initial Machine Info
   let init = Machine.init info in *)
 
-  let init = {all_states = [Q 0 ; Q 1] ; prev_state = Q 0;
-               curr_state = Q 1; next_state = Q 1 ; alphabet = [0;1];
+  let init = {all_states = [Q 0 ; Q 1 ; Q 3 ; Q 4 ; Q 5; Q 6; Q 7 ; Q 8 ; Q 9 ; Q 10 ; Q 11] ; prev_state = Q 0;
+               curr_state = Q 4; next_state = Q 1 ; alphabet = [0;1];
                tape_symbol_read = Sigma 0} in
   let matrix = [((Q 1, "a"), (Q 0, "b", Left)) ;
-                ((Q 1, "b"), (Q 1, "b", Left))] in
+                ((Q 1, "b"), (Q 1, "b", Left)) ;
+                ((Q 3, "c"), (Q 5, "z", Right)) ;
+                ((Q 0, "h"), (Q 6, "h", Right)) ;
+                ((Q 0, "a"), (Q 11, "h", Left)) ;
+                ((Q 0, "z"), (Q 7, "h", Left)) ;
+                ((Q 8, "z"), (Q 0, "h", Left))] in
   (* Create the containing vbox. *)
 
 
@@ -83,6 +89,7 @@ let main () =
   let drawable_i = new GDraw.drawable w in
 
   let paint machine_state matrix =
+
       let (width_i, height_i) = drawable_i#size in
       let (float_width_i, float_height_i) =
       (float_of_int(width_i), float_of_int(height_i)) in
@@ -117,6 +124,10 @@ let main () =
 
         draw_rectangle drawable_i "Spring Green" (!a, !b) state_length
 
+        else if l_state = init.prev_state then
+
+        draw_rectangle drawable_i "Violet" (!a, !b) state_length
+
         else
 
         draw_rectangle drawable_i "Medium Blue" (!a, !b) state_length in
@@ -125,18 +136,27 @@ let main () =
         draw_text drawable_i (a, b) l_string_state ;
         *)
 
+
+
         let float_a = float_of_int(!a) in
         let float_b = float_of_int(!b) in
+
+        let str_sta_x = string_of_float float_a in
+       let str_sta_y = string_of_float float_b in
+       let () = Pervasives.print_string ("x : "^str_sta_x) in
+       let () = Pervasives.print_string ("y : "^str_sta_y) in
 
 
         (* New X Y Coordinates *)
         let (imut_a,imut_b) =
 
-        if float_b >= 0.9*.float_width_i then
+        if float_a >= 0.7*.float_width_i then
+
           (int_of_float(0.1*.float_width_i),
             !b + state_length + int_of_float(0.03*.float_height_i) )
 
         else
+          let () = Pervasives.print_string "new line" in
           (!a + state_length + int_of_float(0.03*.float_width_i), !b)
         in
         a := imut_a ;
@@ -149,8 +169,10 @@ let main () =
 
       let matrix_num = List.length matrix in
 
+      let string_test = draw_text drawable_i (70, 70) font "test" in
+
       for y = 0 to (matrix_num - 1) do
-       let () = Pervasives.print_string "yo" in
+
 
        let ((st_state, in_lett),
             (end_state, out_lett, dir)) = List.nth matrix y in
@@ -161,10 +183,7 @@ let main () =
        let end_state_loc = !st_loc_lst |> List.filter
          (fun x -> x.state = end_state) |> List.hd in
 
-       let str_sta_x = string_of_int st_state_loc.x_cent in
-       let str_sta_y = string_of_int st_state_loc.y_cent in
-       let () = Pervasives.print_string ("x : "^str_sta_x) in
-       let () = Pervasives.print_string ("y : "^str_sta_y) in
+
 
        let () = if st_state = end_state
         then
@@ -176,21 +195,24 @@ let main () =
         in
 
        drawable_i#polygon ~filled: true
-       [(end_state_loc.x_cent - 5, end_state_loc.y_cent + 3) ;
-        (end_state_loc.x_cent + 5, end_state_loc.y_cent + 3) ;
-        (end_state_loc.x_cent, end_state_loc.y_cent - 3)] ;
+       [(end_state_loc.x_cent - 8, end_state_loc.y_cent + 5) ;
+        (end_state_loc.x_cent + 8, end_state_loc.y_cent + 5) ;
+        (end_state_loc.x_cent, end_state_loc.y_cent - 5)] ;
 
        ()
 
       done ;
 
+
+
+
    in
 
   let gui _ = let () = paint init matrix in true in
 
-  (* Button - Stepping Part *)
+  (* Button - Stepping Part
   let button = GButton.button ~label:"Next Step"
-                              ~packing: vbox#add () in
+                              ~packing: vbox#add () in *)
   (* Step
   button#connect#clicked ~callback: (fun () -> paint (step) ); *)
 
