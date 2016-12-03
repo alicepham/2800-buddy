85524617e082cf58d585652fa9a1ed7da902cae6
Ubuntu <ubuntu@csvm>
2016-12-03 13:00:17 -0500

    Added a folder for rsa encryption, defined an mli.

---
+++ rsa/_build/rsa.mli
@@ -0,0 +1,5 @@
+(*Defines the RSA interface, for encrytion of strings*)
+
+val encrypt: string -> string
+
+val decrypt: string -> string
---
+++ rsa/rsa.ml
@@ -0,0 +1,23 @@
+(*Key generation stuff*)
+
+let p = 41
+let q = 83
+let n = 3403
+let phi = 3280
+let public_key = (17, 3403)
+let private_key = (193, 3403)
+
+let keys = ("Public Key is "^ (string_of_int 17),
+  "Private key is "^(string_of_int 193))
+(*Getting digits from a string*)
+
+let string_to_list s =
+  let rec helper c len =
+    if c = len then []
+    else (String.get s c)::(helper (c+1) len)
+  in
+  helper 0 (String.length s)
+
+let list_to_digits lst =
+  List.map (fun x -> Char.code x) lst
+
