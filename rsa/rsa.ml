330d27d6a90b915809a5442df8481499db401fd9
Tavish McDonald <tmm248@cornell.edu>
2016-12-04 20:58:28 -0500

    documented machine and rsa

--- rsa/rsa.ml
+++ rsa/rsa.ml
@@ -47,14 +47,14 @@ let fast_exponentiate a b m =
 (* [sieve n] Creates an array of size [n] where elements are initalized to bools
  * according to the Sieve of Eratosthenes algorithm. To find all prime numbers
  * less than a given integer [n]:
- *   1) Create a list of consecutive integers from 2 through (n-1):(2,3,...,n-1)
+ *  "1) Create a list of consecutive integers from 2 through (n-1):(2,3,...,n-1)
  *   2) Initially, let p = 2, the smallest prime number
  *   3) Enumerate the multiples of p counting to n from 2p in increments of p
  *      and mark them in the list (these will be 2p, 3p, 4p,...,; the p itself
  *      should not be marked)
  *   4) Find the first number greater than p in the list that is not marked. If
  *      there was no such number, stop. Otherwise, let p now equal this new
- *      number (which is the next prime), and repeat from step 3
+ *      number (which is the next prime), and repeat from step 3"
  *
  * the boolean elements of the array created by [sieve n] correspond to false
  * for non-prime values and true for prime values.
